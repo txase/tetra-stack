340251613158fcb97ab5989e49abf89b8a5cb0d0
Eric Betts <bettse@fastmail.fm>
2016-09-11 13:03:25 -0700

    content type detection

--- index.js
+++ index.js
@@ -6,34 +6,39 @@ var pwdgen = require('./PWDGen');
 
 //http://stackoverflow.com/a/12753026/1112230
 require.extensions['.html'] = function htmlRequire(module, filename) {
-	module.exports = fs.readFileSync(filename, 'utf8');
+  module.exports = fs.readFileSync(filename, 'utf8');
 };
 var index = require("./index.html");
 
 module.exports = function handler(message, output, done) {
-	if (message.method == 'GET') {
-	  console.log(`Message: ${(new Date()).toTimeString()} ${message.method}`)
-		done({
-			statusCode: 200,
-			headers: {
-				"Content-Type": "text/html"
-			},
-			body: index
-		})
-	} else if (message.method == 'POST') {
+  if (message.method == 'GET') {
+    console.log(`Message: ${(new Date()).toTimeString()} ${message.method}`)
+    done({
+      statusCode: 200,
+      headers: {
+        "Content-Type": "text/html"
+      },
+      body: index
+    })
+  } else if (message.method == 'POST') {
+    console.log(`Message: ${(new Date()).toTimeString()} ${message.method} ${message.params} ${message.body}`)
+    var pwd = '00000000';
+    if (message.params.headers['Content-Type'] == "application/json") {
+      let body = JSON.parse(message.body);
+      pwd = body.uid;
+    } else if (message.params.headers['Content-Type'].index("application/x-www-form-urlencoded") != -1) {
+
+    }
     /*
     var pwd = pad(8, PWDGen(uid).toString(16), '0');
-    console.log(uid,'=>',pwdgen(uid))
     */
-
-	  var pwd = '00000000';
-	  console.log(`Message: ${(new Date()).toTimeString()} ${message.method} ${message.body}`)
-		done({
-			statusCode: 200,
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({pwd: pwd})
-		})
-	}
+    console.log(uid, '=>', pwdgen(uid));
+    done({
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({pwd: pwd})
+    })
+  }
 }
